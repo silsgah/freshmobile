98cb1b36eda4ed03c5663b34233473b22e981ddb
silsgah <silasgah@yahoo.co.uk>
2014-02-05 00:12:36 -0800

    freshdeal
    new mobile deal

---
+++ scripts/App/app.js
@@ -0,0 +1,18 @@
+﻿(function (global) {
+	
+	document.addEventListener('deviceready', function () {
+		navigator.splashscreen.hide();
+	}, false);
+
+	var app = new kendo.mobile.Application(document.body, { transition: "slide", layout: "jitterz-default" });
+
+	
+})(window);
+
+
+
+
+
+
+
+
